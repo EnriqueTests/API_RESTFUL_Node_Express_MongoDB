6c838b93d6a434de3113690c0e19e85f03ab02a9
EnriqueTests <enriquenada@outlook.es>
2021-07-28 15:05:48 -0500

    Error 500

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let app = express()
 
 let personRoute = require('./routes/person')
 
+let path = require('path')
+
 app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`)
     next()
@@ -15,6 +17,12 @@ app.use(express.static('public'))
 // Handler for 404 - Resource Not Found
 app.use((req, res, next) => {
     res.status(404).send('We think you are lost!')
+})
+
+// Handler for Error 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
 })
 
 const PORT = process.env.PORT || 3000
