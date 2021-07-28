fabb9be3e41951fa2cac2a373340df9787f3d28d
EnriqueTests <enriquenada@outlook.es>
2021-07-28 14:37:28 -0500

    Error 404

--- src/index.js
+++ src/index.js
@@ -12,5 +12,10 @@ app.use((req, res, next) => {
 app.use(personRoute)
 app.use(express.static('public'))
 
+// Handler for 404 - Resource Not Found
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost!')
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`))
